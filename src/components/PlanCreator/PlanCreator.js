063aa97be169e6cc866d9c971b163e8ca06d4d3b
or <or@smarthead.ru>
2021-01-04 02:06:18 +0300

    add PlanCreator features

--- src/components/PlanCreator/PlanCreator.js
+++ src/components/PlanCreator/PlanCreator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 // import axios from 'axios';
 import TrainingDay from "../TrainingDay/TrainingDay";
 
@@ -7,8 +7,47 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/core";
+import Exercise from "../TrainingDay/Exercise";
+import Paper from "@material-ui/core/Paper/Paper";
 
 function PlanCreator() {
+
+    const [dayNumber, setDay] = React.useState([]);
+
+    const addDay = () => {
+        setDay([...dayNumber, {}]);
+    };
+
+    const deleteDay = (index) => {
+        setDay(
+            [...dayNumber].filter((elem, i) => {
+                return i !== index;
+            })
+        );
+    };
+
+    const useStyles = makeStyles((theme) => ({
+
+        paper: {
+            padding: theme.spacing(2),
+            textAlign: "center",
+            color: theme.palette.text.secondary,
+        },
+        button: {
+            margin: theme.spacing(2),
+            // marginBottom: theme.spacing(2),
+        },
+        delButton: {
+            margin: theme.spacing(2),
+            position: 'absolute',
+
+        }
+    }));
+
+    const classes = useStyles();
+
   return (
     <div>
       <Box>
@@ -17,7 +56,33 @@ function PlanCreator() {
             Заполнение плана
           </Typography>
           <Grid container spacing={3}>
-            <TrainingDay />
+
+              {dayNumber.map((day, i) => {
+                  return (
+                      <Grid item xs={12} sm={4}>
+                          <Paper className={classes.paper}>
+                              <Typography variant="h5" component="h3" align="center">
+                                  {i + 1} тренировочный день
+                              </Typography>
+                              <Button
+                                  variant="contained"
+                                  color="secondary"
+                                  onClick={() => deleteDay(i)}
+                              >
+                                  {" "}
+                                  Удалить день{" "}
+                              </Button>
+                              <Fragment key={`day_${i}`} className={classes.day}>
+                                  <TrainingDay/>
+                              </Fragment>
+                          </Paper>
+                      </Grid>
+                  );
+              })}
+
+              <Button variant="contained" color="primary" className={classes.button} onClick={addDay}>
+                  +
+              </Button>
           </Grid>
         </Container>
       </Box>
--- src/components/TrainingDay/TrainingDay.js
+++ src/components/TrainingDay/TrainingDay.js
@@ -5,18 +5,12 @@ import axios from "axios";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     flexWrap: "wrap",
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
   },
 }));
 
@@ -39,34 +33,27 @@ function TrainingDay() {
 
   return (
     <Fragment>
-      <Grid item xs={12} sm={4}>
-        <Paper className={classes.paper}>
-          <Typography variant="h5" component="h3" align="center">
-            1 тренировочный день
-          </Typography>
 
-          {exercisesNumber.map((exercise, i) => {
-            return (
-              <Fragment key={`exercise_${i}`}>
-                <Exercise number={i + 1} />
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  onClick={() => deleteExercise(i)}
-                >
-                  {" "}
-                  Удалить{" "}
-                </Button>
-              </Fragment>
-            );
-          })}
+      {exercisesNumber.map((exercise, i) => {
+        return (
+          <Fragment key={`exercise_${i}`}>
+            <Exercise number={i + 1} />
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => deleteExercise(i)}
+            >
+              {" "}
+              Удалить движение{" "}
+            </Button>
+          </Fragment>
+        );
+      })}
 
-          <hr />
-          <Button variant="contained" onClick={addExercise}>
-            Добавить движение
-          </Button>
-        </Paper>
-      </Grid>
+      <hr />
+      <Button variant="contained" onClick={addExercise}>
+        Добавить движение
+      </Button>
     </Fragment>
   );
 }
